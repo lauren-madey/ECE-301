b367471dbebb9c2ba22ab998446d17c51a5caee0
Lauren Madey <elizabeth.lauren.441@gmail.com>
2021-04-06 15:44:50 -0400

    Add files via upload

---
+++ ece301hw8/rect.m
@@ -0,0 +1,9 @@
+%  rect.m
+function y= rect(t)
+if -0.5<=t<=0.5
+    y=1;
+else
+    y=0;
+end
+return
+
---
+++ ece301hw8/tria.m
@@ -0,0 +1,9 @@
+%   tria.m
+function y= tria(t)
+if -1<=t<=1
+    y=1-abs(t);
+else
+    y=0;
+end
+end
+
